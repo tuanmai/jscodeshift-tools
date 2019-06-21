61b19faf9d04c353aaec1ecaecf0a3b01f344432
Tuan Mai <perhuman@gmail.com>
2019-06-21 10:20:52 +0700

    refactor

--- changeTextToHeroDesign.js
+++ changeTextToHeroDesign.js
@@ -4,16 +4,21 @@ import {
   findRelativeImport
 } from "./importUtils";
 
+const migrateText = (j, root, relativePath, newPath) => {
+  const commonTextImport = findRelativeImport(j, root, relativePath);
+  const defaultImportName = findImportDefaultSpecifier(j, commonTextImport);
+  if (commonTextImport.length < 1) return file.source;
+  commonTextImport.replaceWith(() => {
+    return createImportDefaultNode(j, defaultImportName, [], newPath);
+  });
+};
+
 export default (file, api) => {
   const j = api.jscodeshift;
   const root = j(file.source);
-  const commonTextImport = findRelativeImport(j, root, "common/text");
-  const defaultImportName = findImportDefaultSpecifier(j, commonTextImport);
-  if (commonTextImport.length < 1) return file.source;
-  console.log("Found import text");
-  commonTextImport.replaceWith(() => {
-    return createImportDefaultNode(j, defaultImportName, [], "hero-design-rn");
-  });
+  migrateText(j, root, "common/text", "hero-design-rn");
+  migrateText(j, root, "./text", "hero-design-rn");
+  migrateText(j, root, "../text", "hero-design-rn");
 
   return root.toSource({ quote: "single" });
 };
