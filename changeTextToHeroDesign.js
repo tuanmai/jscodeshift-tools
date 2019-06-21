edcfc791e3e75453b7fa4cea88edcbdd0fba3eee
Tuan Mai <perhuman@gmail.com>
2019-06-21 10:27:35 +0700

    Fix error when file is not import text from 'common/text'

--- changeTextToHeroDesign.js
+++ changeTextToHeroDesign.js
@@ -7,18 +7,21 @@ import {
 const migrateText = (j, root, relativePath, newPath) => {
   const commonTextImport = findRelativeImport(j, root, relativePath);
   const defaultImportName = findImportDefaultSpecifier(j, commonTextImport);
-  if (commonTextImport.length < 1) return file.source;
+  if (commonTextImport.length < 1) return false;
   commonTextImport.replaceWith(() => {
     return createImportDefaultNode(j, defaultImportName, [], newPath);
   });
+  return true;
 };
 
 export default (file, api) => {
   const j = api.jscodeshift;
   const root = j(file.source);
-  migrateText(j, root, "common/text", "hero-design-rn");
-  migrateText(j, root, "./text", "hero-design-rn");
-  migrateText(j, root, "../text", "hero-design-rn");
+  let found = false;
+  found = found || migrateText(j, root, "common/text", "hero-design-rn");
+  found = found || migrateText(j, root, "./text", "hero-design-rn");
+  found = found || migrateText(j, root, "../text", "hero-design-rn");
+  if (!found) return file.source;
 
   return root.toSource({ quote: "single" });
 };
